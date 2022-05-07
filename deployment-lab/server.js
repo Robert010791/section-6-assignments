799344ccad6d47afe0735e20f47df7eaa67ce094
Robert Robbins <robertrobbins321@msn.om>
2022-05-06 19:10:08 -0600

    Added get '/'

--- deployment-lab/server.js
+++ deployment-lab/server.js
@@ -4,6 +4,10 @@ const path = require('path');
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
 const port = process.env.PORT || 3030;
 
 app.listen(port, () => console.log(`Server coming in on port ${port}`));
